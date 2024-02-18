a904055f7e7e65ce07ba201967e67caea764ed92
Sandesh <sandeshsg6@gmail.com>
2024-02-18 22:07:08 +0530

    zz

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -49,7 +49,9 @@ const HeroSection = () => {
                     offset={-100}
                     duration={500}
                 >
-                    <React.Fragment><HiArrowDown size={35} className="animate-bounce" /></React.Fragment>
+                    <div>
+                        <HiArrowDown size={35} className="animate-bounce" />
+                    </div>
                 </Link>
             </div>
         </section>
